42b2f7ba28f88745a91f338fb8bb31c792a0a01e
龍矢 瀧上 <uttne@outlook.com>
2017-04-07 00:55:45 +0900

    Added PutIn test code.

--- uttne.Linq.standard10/uttne.Linq.standard10.Tests/EnumerableTests.cs
+++ uttne.Linq.standard10/uttne.Linq.standard10.Tests/EnumerableTests.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace uttne.Linq.standard10.Tests
 {
     public class EnumerableTests
     {
+        [Fact]
+        public void PutInTest()
+        {
+            var source = new List<string>()
+            {
+                "aaaa",
+                "bbbb",
+                "cccc",
+                "dddd",
+                "eeee",
+                "ffff",
+            };
 
+            var result = source.PutIn("gggg").ToList();
+
+            var target = new List<string>()
+            {
+                "aaaa",
+                "bbbb",
+                "cccc",
+                "dddd",
+                "eeee",
+                "ffff",
+                "gggg",
+            };
+
+            Xunit.Assert.Equal(target,result);
+        }
     }
 }
