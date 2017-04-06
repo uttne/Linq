a9735725245e934de8cb204594af398ef312f7d2
龍矢 瀧上 <uttne@outlook.com>
2017-04-07 01:06:04 +0900

    Added test code of overridden PutIn with argument index.

--- uttne.Linq.standard10/uttne.Linq.standard10.Tests/EnumerableTests.cs
+++ uttne.Linq.standard10/uttne.Linq.standard10.Tests/EnumerableTests.cs
@@ -36,5 +36,87 @@ namespace uttne.Linq.standard10.Tests
 
             Xunit.Assert.Equal(target,result);
         }
+
+        [Fact]
+        public void PutInThatHasArgumentIndexTest()
+        {
+            var source = new List<string>()
+            {
+                "aaaa",
+                "bbbb",
+                "cccc",
+                "dddd",
+                "eeee",
+                "ffff",
+            };
+
+            {
+                var result = source.PutIn(0,"gggg").ToList();
+
+                var target = new List<string>()
+                {
+                    "gggg",
+                    "aaaa",
+                    "bbbb",
+                    "cccc",
+                    "dddd",
+                    "eeee",
+                    "ffff",
+                };
+
+                Xunit.Assert.Equal(target, result);
+            }
+
+            {
+                var result = source.PutIn(-1,"gggg").ToList();
+
+                var target = new List<string>()
+                {
+                    "gggg",
+                    "aaaa",
+                    "bbbb",
+                    "cccc",
+                    "dddd",
+                    "eeee",
+                    "ffff",
+                };
+
+                Xunit.Assert.Equal(target, result);
+            }
+
+            {
+                var result = source.PutIn(2, "gggg").ToList();
+
+                var target = new List<string>()
+                {
+                    "aaaa",
+                    "bbbb",
+                    "gggg",
+                    "cccc",
+                    "dddd",
+                    "eeee",
+                    "ffff",
+                };
+
+                Xunit.Assert.Equal(target, result);
+            }
+
+            {
+                var result = source.PutIn(10, "gggg").ToList();
+
+                var target = new List<string>()
+                {
+                    "aaaa",
+                    "bbbb",
+                    "cccc",
+                    "dddd",
+                    "eeee",
+                    "ffff",
+                    "gggg",
+                };
+
+                Xunit.Assert.Equal(target, result);
+            }
+        }
     }
 }
